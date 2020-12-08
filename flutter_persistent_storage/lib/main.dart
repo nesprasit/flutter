daa1416724f5a2de88657f149e238b85d44637a9
Prasit.s <Prasit.s@clicknext.com>
2020-12-08 13:38:28 +0700

    no message

--- flutter_persistent_storage/lib/main.dart
+++ flutter_persistent_storage/lib/main.dart
@@ -1,17 +1,28 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_persistent_storage/ui/home.dart';
 
 void main() {
-  runApp(App());
+  runApp(AppAndroid());
 }
 
-class App extends MaterialApp {
-
+class AppAndroid extends MaterialApp {
   @override
   bool get debugShowCheckedModeBanner => false;
-  
+
   @override
   ThemeData get theme => ThemeData();
+
+  @override
+  Widget get home => Home();
+}
+
+class AppIOS extends CupertinoApp {
+  @override
+  bool get debugShowCheckedModeBanner => false;
+
+  @override
+  CupertinoThemeData get theme => CupertinoThemeData();
 
   @override
   Widget get home => Home();
